33f7dcc6d814338b0f07b36a8098bd6bc5efdbd4
Arnav Chawla <arnavchawla23@gmail.com>
2020-06-16 08:52:45 -0700

    PatternMatching passes all 3 test cases

--- 2020 round 1A/patternmatching.py
+++ 2020 round 1A/patternmatching.py
@@ -3,14 +3,22 @@ def compute(case):
     strings = []
     prefixes = []
     suffixes = []
+    middle = []
     for i in range(0,number):
         strings.append(input())
-    for test in strings:
-        new = test.split("*")
-        if new[0] != "":
+    for string in strings:
+        new = string.split("*")
+        if string.count("*") == 1:
+            if new[0] != "":
+                prefixes.append(new[0])
+            if new[1] != "":
+                suffixes.append(new[1])
+        else:
             prefixes.append(new[0])
-        if new[1] != "":
-            suffixes.append(new[1])
+            suffixes.append(new[-1])
+            for i in range(1,len(new)-1):
+                middle.append(new[i])
+
 
     prefixes.sort(key = len)
     prefixes.reverse()
@@ -38,7 +46,7 @@ def compute(case):
                     return "Case #{}: *".format(case)
     except:
         pass
-    return "Case #{}: {}{}".format(case,"".join(basepre[x] for x in range(0,len(basepre))),"".join(basesuff[x] for x in range(0,len(basesuff))))
+    return "Case #{}: {}{}{}".format(case,"".join(basepre[x] for x in range(0,len(basepre))),"".join(item for item in middle),"".join(basesuff[x] for x in range(0,len(basesuff))))
 if __name__ == "__main__":
     cases = int(input())
     for i in range(1,cases+1):
